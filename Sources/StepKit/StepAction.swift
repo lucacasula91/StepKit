7c710f1a79f1962eddb32a6e87cb8d5ee3cc9ca2
Luca Casula <luca.casula@it.ey.com>
2023-04-29 17:07:22 +0200

    Created StepAction class that descibe the step forward behavior.

---
+++ Sources/StepKit/StepAction.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+/// Specify the behavior that guide the flow step forward.
+enum StepAction {
+
+    /// Button object that allow to make a step forward.
+    ///
+    /// By default `title` property is setted to nil. When `nil` value is passed to `title` property the StepView object will show **Next** as default title.
+    ///
+    /// - parameter title: The title to assign to the button.
+    case button(title: String? = nil)
+    
+    /// Checkbox button that allow to mark a step as completed.
+    ///
+    /// By default `title` property is setted to nil. When `nil` value is passed to `title` property the StepView object will show **Mark as completed** as default title.
+    ///
+    /// - parameter title: The title to assign to the checkbox.
+    case checkBox(title: String? = nil)
+    
+    /// Timer specified in second that allow to make a step forward once the countdown is completed.
+    ///
+    /// Once the count down is completed the text **Completed** is shown instead of the counter.
+    case timer(seconds: TimeInterval)
+}
