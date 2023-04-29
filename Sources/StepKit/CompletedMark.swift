149d54b620b980f8d4dfb2ab285e06ea05340222
Luca Casula <luca.casula@it.ey.com>
2023-04-29 18:22:37 +0200

    Create CompletedMark view to represent the completion of a specific step

---
+++ Sources/StepKit/CompletedMark.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+
+/// Represent a completion mark that consist in a **Completed** text followed by a green check mark.
+///
+/// CompletedMark view expose the `isVisible` boolean parameter with drives it's visibility.
+struct CompletedMark: View {
+    var isVisible: Bool
+    
+    var body: some View {
+        if isVisible {
+            VStack {
+                HStack {
+                    Text("Completed")
+                        .fontWeight(.bold)
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.green)
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct CompletedMark_Previews: PreviewProvider {
+    static var previews: some View {
+        CompletedMark(isVisible: true)
+    }
+}
